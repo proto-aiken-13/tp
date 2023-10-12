6f63f4d17c055ad7093a1c554c222753e9024c16
Choonyan02 <choonyan02@gmail.com>
2023-10-13 01:06:48 +0800

    Update PersonBuilder

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import seedu.address.model.fields.Comment;
 import seedu.address.model.fields.Tag;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.Attendance;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
@@ -21,11 +22,13 @@ public class PersonBuilder {
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+    public static final String DEFAULT_ATTENDANCE = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,";
 
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
+    private Attendance attendance;
     private Set<Tag> tags;
     private Set<Comment> comments;
 
@@ -37,6 +40,7 @@ public class PersonBuilder {
         phone = new Phone(DEFAULT_PHONE);
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
+        attendance = new Attendance(DEFAULT_ATTENDANCE);
         tags = new HashSet<>();
         comments = new HashSet<>();
     }
@@ -49,6 +53,7 @@ public class PersonBuilder {
         phone = personToCopy.getPhone();
         email = personToCopy.getEmail();
         address = personToCopy.getAddress();
+        attendance = personToCopy.getAttendance();
         tags = new HashSet<>(personToCopy.getTags());
         comments = new HashSet<>(personToCopy.getComments());
     }
@@ -86,6 +91,14 @@ public class PersonBuilder {
     }
 
     /**
+     * Sets the {@code Attendance} of the {@code Person} that we are building.
+     */
+    public PersonBuilder withAttendance(String attendance) {
+        this.attendance = new Attendance(attendance);
+        return this;
+    }
+
+    /**
      * Sets the {@code Phone} of the {@code Person} that we are building.
      */
     public PersonBuilder withPhone(String phone) {
@@ -102,7 +115,7 @@ public class PersonBuilder {
     }
 
     public Person build() {
-        return new Person(name, phone, email, address, tags, comments);
+        return new Person(name, phone, email, address, attendance, tags, comments);
     }
 
 }
