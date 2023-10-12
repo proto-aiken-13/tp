c7183f9b25f981fc49c74aa193202d821d378d43
Choonyan02 <choonyan02@gmail.com>
2023-10-13 02:58:18 +0800

    Fix 1 test case

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -169,7 +169,7 @@ public class EditCommand extends Command {
          * Returns true if at least one field is edited.
          */
         public boolean isAnyFieldEdited() {
-            return CollectionUtil.isAnyNonNull(name, phone, email, address, attendance, tags, comments);
+            return CollectionUtil.isAnyNonNull(name, phone, email, address, tags, comments);
         }
 
         public void setName(Name name) {
