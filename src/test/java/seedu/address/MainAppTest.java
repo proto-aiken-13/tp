32071ad430a5e837c4d69e36d93a5533a859e6fb
leesiyuan <lee.si.yuan.20@gmail.com>
2023-10-15 17:34:19 +0800

    Create MainAppTest.java
    Added testing for initialisaiton of main application class.

---
+++ src/test/java/seedu/address/MainAppTest.java
@@ -0,0 +1,11 @@
+package seedu.address;
+
+import org.junit.jupiter.api.Test;
+
+public class MainAppTest {
+    @Test
+    public void main_Test_initialised() {
+        MainApp main = new MainApp();
+        assert main != null;
+    }
+}
