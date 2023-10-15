bdaf90e552e40b3106d8eebd7cff05dd31cd75d2
leesiyuan <lee.si.yuan.20@gmail.com>
2023-10-15 17:34:34 +0800

    Create MainTest.java
    Added testing for main.class.

---
+++ src/test/java/seedu/address/MainTest.java
@@ -0,0 +1,11 @@
+package seedu.address;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void main_Test_initialised() {
+        Main main = new Main();
+        assert main != null;
+    }
+}
