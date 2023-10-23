9abc03fd9901dcd08d2a82b7c5f95da4437a598a
leesiyuan <lee.si.yuan.20@gmail.com>
2023-10-24 02:06:28 +0800

    Update HelpCommand.java
    Adhered to checkstyles.

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -13,18 +13,18 @@ public class HelpCommand extends Command {
             + "Example: " + COMMAND_WORD;
 
     public static final String SHOWING_HELP_MESSAGE =
-            "Here are the list of commands:\n" +
-            "a. ADD COMMAND\n" + AddCommand.MESSAGE_USAGE + "\n" +
-            "b. CLEAR COMMAND\n" + ClearCommand.MESSAGE_USAGE + "\n" +
-            "c. DELETE COMMAND\n" + DeleteCommand.MESSAGE_USAGE + "\n" +
-            "d. EDIT COMMAND\n" + EditCommand.MESSAGE_USAGE + "\n" +
-            "e. EXIT COMMAND\n" + ExitCommand.MESSAGE_USAGE + "\n" +
-            "f. FIND COMMAND\n" + FindCommand.MESSAGE_USAGE + "\n" +
-            "g. HELP COMMAND\n" + HelpCommand.MESSAGE_USAGE + "\n" +
-            "h. LIST COMMAND\n" + ListCommand.MESSAGE_USAGE + "\n" +
-            "i. MARK ATTENDANCE COMMAND\n" + MarkAttendanceCommand.MESSAGE_USAGE + "\n" +
-            "j. UNMARK ATTENDANCE COMMAND\n" + UnmarkAttendanceCommand.MESSAGE_USAGE + "\n" +
-                    "k. ASSIGN COMMAND\n" + AssignmentCommand.MESSAGE_USAGE + "\n";
+            "Here are the list of commands:\n"
+                    + "a. ADD COMMAND\n" + AddCommand.MESSAGE_USAGE + "\n"
+                    + "b. CLEAR COMMAND\n" + ClearCommand.MESSAGE_USAGE + "\n"
+                    + "c. DELETE COMMAND\n" + DeleteCommand.MESSAGE_USAGE + "\n"
+                    + "d. EDIT COMMAND\n" + EditCommand.MESSAGE_USAGE + "\n"
+                    + "e. EXIT COMMAND\n" + ExitCommand.MESSAGE_USAGE + "\n"
+                    + "f. FIND COMMAND\n" + FindCommand.MESSAGE_USAGE + "\n"
+                    + "g. HELP COMMAND\n" + HelpCommand.MESSAGE_USAGE + "\n"
+                    + "h. LIST COMMAND\n" + ListCommand.MESSAGE_USAGE + "\n"
+                    + "i. MARK ATTENDANCE COMMAND\n" + MarkAttendanceCommand.MESSAGE_USAGE + "\n"
+                    + "j. UNMARK ATTENDANCE COMMAND\n" + UnmarkAttendanceCommand.MESSAGE_USAGE + "\n"
+                    + "k. ASSIGN COMMAND\n" + AssignmentCommand.MESSAGE_USAGE + "\n";
     @Override
     public CommandResult execute(Model model) {
         return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
