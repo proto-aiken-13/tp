ad1f3954d7f7782bde747b360dc1116e3fa8987e
leesiyuan <lee.si.yuan.20@gmail.com>
2023-10-24 10:43:14 +0800

    Update UiUtil.java
    Added Assertion.

--- src/main/java/seedu/address/ui/UiUtil.java
+++ src/main/java/seedu/address/ui/UiUtil.java
@@ -4,6 +4,7 @@ import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.logging.Logger;
 
 /**
  * Utility class for UI components.
@@ -13,8 +14,12 @@ public class UiUtil {
      * Opens a link using the default web browser on the user's system.
      * @param url The URL to be opened in the web browser.
      */
+    private static final Logger logger = Logger.getLogger(UiUtil.class.getName());
     public static void open(String url) {
         String os = System.getProperty("os.name").toLowerCase();
+        assert !os.equals("");
+        logger.info("Opening link: " + url);
+
         try {
             if (os.contains("win") || os.contains("mac")) {
                 Desktop.getDesktop().browse(new URI(url));
