b16ae3ab1bfa93f98f9d8688e8bf4e71ff1709ad
leesiyuan <lee.si.yuan.20@gmail.com>
2023-10-26 01:56:19 +0800

    Create assignGroup command
    Created assignment by group.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -10,6 +10,7 @@ import java.util.regex.Pattern;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.AssignmentCommand;
+import seedu.address.logic.commands.AssignmentGroupCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CreateGroupCommand;
@@ -67,6 +68,9 @@ public class AddressBookParser {
 
         case AssignmentCommand.COMMAND_WORD:
             return new AssignmentCommandParser().parse(arguments);
+
+        case AssignmentGroupCommand.COMMAND_WORD:
+            return new AssignmentGroupCommandParser().parse(arguments);
 
         case CreateGroupCommand.COMMAND_WORD:
             return new CreateGroupCommandParser().parse(arguments);
